088a78d5c15f2f6082cdced0247155620e8e8dc0
matheusladislau <matheusladislauribeiro@gmail.com>
2018-06-25 01:12:08 -0300

    First commit

---
+++ src/binarytreejava/Main.java
@@ -0,0 +1,13 @@
+package binarytreejava;
+/**
+ * 
+ * @author Matheus Ladislau Ribeiro
+ * matheusladislauribeiro@gmail.com
+ * www.linkedin.com/in/matheusladislau/
+ * 
+ */
+public class Main{
+    public static void main(String[] args) {
+    }
+    
+}
